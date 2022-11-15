5f626c9513c7933d667d48116b788f781eec55a7
rahuldhirendersingh <rahuldhirendersingh@gmail.com>
2022-11-15 15:35:48 +0530

    data types in cpp

---
+++ 03-data-types/datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+  int yearOfBirth = 2002;
+  char gender = 'm';
+  bool isAdult = true;
+  float averageGrade = 4.5;
+  double balance = 62565548;
+  
+  cout << yearOfBirth << endl;
+  cout << gender << endl;
+  cout << isAdult << endl;
+  cout << averageGrade << endl;
+  cout << balance << endl;
+
+}
